2cbc91ca190fd20b473a98cfa5e79ff22b020d05
lordsayur <umar_317@hotmail.com>
2021-12-06 14:29:45 +0800

    ✨ Camera: implement rotate logic

--- Water Taxi Tycoon/Assets/Scripts/TopDownCamera.cs
+++ Water Taxi Tycoon/Assets/Scripts/TopDownCamera.cs
@@ -16,10 +16,23 @@ public class TopDownCamera : MonoBehaviour
     void LateUpdate()
     {
         FollowPlayer();
+        RotateCamera();
     }
 
     private void FollowPlayer()
     {
         transform.position = player.position + separationDistance;
+    }
+
+    private void RotateCamera()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            transform.Rotate(0f, -90f, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            transform.Rotate(0f, 90f, 0f);
+        }
     }
 }
